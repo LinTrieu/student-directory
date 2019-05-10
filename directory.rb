321ada969a15b98ec364094d98bb1188ce02e496
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-10 15:54:28 +0100

    completed task 7

--- directory.rb
+++ directory.rb
@@ -27,6 +27,21 @@ students = [
     puts "Overall, we have #{names.count} great students"
   end
 
+  def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+  end
+
+  students = input_students
   print_header
   print(students)
   print_footer(students)
+
