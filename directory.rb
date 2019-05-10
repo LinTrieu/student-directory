c8ee8d3003bbe1ea6bc1824b611d87ccd860e818
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-10 10:33:16 +0100

    new comments to students

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have"
+# it's important that print() doesn't add new line characters
+print 11
+puts " great students"
